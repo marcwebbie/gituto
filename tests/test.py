e648c595e3a79fe069f30bd9eeeccc122eea9483
marcwebbie <marcwebbie@gmail.com>
2015-02-13 12:54:34 -0200

    Fix test for equality

--- tests/test.py
+++ tests/test.py
@@ -2,7 +2,7 @@ import unittest
 
 class Tests(unittest.TestCase):
     def test_equals(self):
-        self.assertEqual(1, 2)
+        self.assertEqual(1, 1)
 
 if __name__ == "__main__":
     unittest.main()
